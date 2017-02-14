5da73c6679e6780313cdaf42ae4b246ecb5e9380
Brennan Sedivy <sedivy94@gmail.com>
2017-02-14 13:18:46 -0600

    Fixing missing commits?

--- chromagraph.py
+++ chromagraph.py
@@ -1,43 +1,43 @@
-class Node:
-	def __init__(self, name, neighbors=[]):
-		self.name = name
-		self.neighbors = neighbors
+def __init__(self, name, neighbors=[]):
+	self.name = name
+	self.neighbors = neighbors
 	def add_neighbor(self, neighbor):
-		if  isinstance(neighbor, Node):
+		if	isinstance(neighbor, Node):
 			self.neighbors.append(neighbor)
 		elif ininstance(neighbor, list):
 			self.neighbors.extend(neighbor)
 		else:
 			print("Invalid argument. Object is not a Node or list of Nodes.")
-		return
-	def remove_neighbor(self, neighbor):
-		if  isinstance(neighbor, str):
-			self.neighbors = [x for x in self.neighbors if x.name != neighbor]
-		elif ininstance(neighbor, list):
-			self.neighbors = self.neighbors = [x for x in self.neighbors for y in neighbor if x != y]	
-		else:
-			print("Invalid argument. Object is not a string or list of strings.")
-		return
-	def rename(self, name):
-		self.name = name
+				return
+		def remove_neighbor(self, neighbor):
+			if	isinstance(neighbor, str):
+				self.neighbors = [x for x in self.neighbors if x.name != neighbor]
+			elif ininstance(neighbor, list):
+				self.neighbors = self.neighbors = [x for x in self.neighbors for y in neighbor if x != y]	
+			else:
+				print("Invalid argument. Object is not a string or list of strings.")
+				return
+		def rename(self, name):
+			self.name = name
 
 class Graph:
 	def __init__(self):
 		self.node_set = set()
-		self.lookup = dict()
-	def add_node(self, node, neighbors=[]):
-		if isinstance(node, str):
-			new_node = Node(node, neighbors)
-		elif isinstance(node, Node):
-			new_node = node
-		node_set.add(new_node)
-		lookup[new_node.name] = new_node
-	def remove_node(self, node):
-		if isinstance(node, str):
-			old_node = lookup[node]
-			node_set.remove(old_node)
-			lookup[node] = None
-		elif isinstance(node, Node):
-			node_set.remove(node)
-			lookup[node.name] = None
-		
+			self.lookup = dict()
+		def add_node(self, node, neighbors=[]):
+			if isinstance(node, str):
+				new_node = Node(node, neighbors)
+			elif isinstance(node, Node):
+				new_node = node
+			else:
+				node_set.add(new_node)
+
+				lookup[new_node.name] = new_node
+		def remove_node(self, node):
+			if isinstance(node, str):
+				old_node = lookup[node]
+				node_set.remove(old_node)
+				lookup[node] = None
+			elif isinstance(node, Node):
+				node_set.remove(node)
+				lookup[node.name] = None	
--- version_sort.py
+++ version_sort.py
@@ -2,13 +2,16 @@ def answer(array):
     # Split major/minor/revision into indeces
     l = list(array)
     if l:
+        # Convert version strings to version arrays
         for index,string in enumerate(l):
             l[index] = [int(x) for x in string.split(".")]
             # Ensures that shorter versions sort higher
             while len(l[index]) < 3:
                 l[index].append(None) # None < 0
-        print version_sort(l)
-        return
+        # Call sorting algorithm on new version array
+        output = version_sort(l)
+        print output
+        return output
     else:
         print "Invalid input. Exiting."
         quit()
@@ -16,44 +19,57 @@ def answer(array):
 def version_sort(array):
     # Merge sort customized for this 2D list
     array_len = len(array)
+    # Cannot divide further
     if array_len <= 1: 
         return array
     middle = array_len // 2
     left = version_sort(array[:middle])
     right = version_sort(array[middle:])
     merged = []
+    # Data needed to iterate through sorted lists
+    # Potentially wasteful
     len_left = len(left)
     len_right = len(right)
     i,j = 0,0
     ver_a = None
     ver_b = None
     ver_small = None
+    # Potentially shakey logic
     while True: 
+        # If left value was not appended to merge
         if (not ver_a) and i < len_left:
             ver_a = left[i]
             i += 1
+        # If right value was not appended to merge
         if (not ver_b) and j < len_right: 
             ver_b = right[j]
             j += 1
+        # Choose smallest of two versions
         ver_small = smallest(ver_a,ver_b)
         if ver_small:
             merged.append(ver_small)
+            # Open up left or right placeholders
+            # Ordering matters! Considers edge case in smallest()
             if ver_small == ver_a:
                 ver_a = None
             elif ver_small == ver_b:
                 ver_b = None
+        # When left and right values have been exhausted
         else:
             return merged
 
 def smallest(ver_a = [], ver_b = []):
     if ver_a and ver_b:
+        # Using zip to make iterative tuples?
         for a,b in zip(ver_a,ver_b):
             if a < b:
                 return ver_a
             elif a > b:
                 return ver_b
         else:
+            # Edge case: When both are equal, return left
             return ver_a
+    # In case of shitty method parameters
     elif ver_a:
         return ver_a
     elif ver_b:
